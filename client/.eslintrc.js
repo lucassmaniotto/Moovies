fdc40431135dc6a2be66c4ca1c38de6d4ce5aa7b
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-03 09:55:36 -0300

    feat: iniciando estrutura do Frontend

---
+++ client/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'global-require': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    camelcase: 'off',
+    'import/no-unresolved': 'off',
+    'no-nested-ternary': 'off',
+    'no-shadow': 'off',
+  },
+};
